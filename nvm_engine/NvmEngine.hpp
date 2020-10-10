8c86cd595347b7e4d6b7bab746355013afbaf8f4
JYLeeLYJ <JYLeeLYJ@qq.com>
2020-10-10 19:32:30 +0800

    [update] begin for season2

--- nvm_engine/NvmEngine.hpp
+++ nvm_engine/NvmEngine.hpp
@@ -9,7 +9,6 @@
 #include <atomic>
 
 #include "include/db.hpp"
-#include "include/bloom_filter.hpp"
 
 class NvmEngine : DB {
 public:
@@ -26,60 +25,7 @@ public:
     ~NvmEngine();
 
 private:
-    std::pair<uint32_t , uint32_t> search(const Slice & key , uint64_t hash);
-    bool append(const Slice & key , const Slice & value , uint32_t i , uint64_t hash);
-
-private:
-
-    uint32_t allocate_index(uint32_t bucket_id){
-        uint32_t index = ++entry_seq[bucket_id];
-        return index < ENTRY_PER_BUCKET ? index + bucket_id * ENTRY_PER_BUCKET : 0;
-    }
-    bool is_valid_index(uint32_t i) {
-        return i && i < BUCKET_MAX; 
-    }
-    uint32_t allocated_bucket_id(){
-        return (thread_seq ++ ) % WRITE_BUCKET;
-    }
-
-private:/* static definition */
-
-    struct entry_t {
-        char key[16];
-        char value[80];
-    };
-
-    #ifdef LOCAL_TEST
-    static constexpr size_t NVM_SIZE = 2*1024*1024*sizeof(entry_t);
-    static constexpr size_t DRAM_SIZE = 16 * 1024 * 1024;       //16M
-    static constexpr size_t FILTER_SIZE = 1 * 1024 * 1024;
-    #else
-    static constexpr size_t NVM_SIZE = 79456894976;
-    static constexpr size_t DRAM_SIZE = 4000000000;
-    static constexpr size_t FILTER_SIZE = 600ULL * 1024 * 1024 * 8;
-    #endif
-
-    //load factor ~ 0.73
-    static constexpr uint32_t ENTRY_MAX = NVM_SIZE / sizeof(entry_t);       //74G / 80 entries
-    static constexpr uint32_t BUCKET_MAX = DRAM_SIZE / sizeof(std::atomic<uint32_t>);    //1G buckets
-    static constexpr uint32_t WRITE_BUCKET = 16;
-    static constexpr uint32_t ENTRY_PER_BUCKET = ENTRY_MAX / WRITE_BUCKET;
-
-private: /* data members */
-
-    entry_t *entry{};
-    std::atomic<uint32_t> *bucket{};
-
-    alignas(CACHELINE_SIZE) 
-    std::array<align_intergral_t<uint32_t , CACHELINE_SIZE>, WRITE_BUCKET> entry_seq{};
-    std::atomic<uint32_t> thread_seq{0};
-    //256M
-    bitmap_filter<FILTER_SIZE> bitset{};
-
-private: /* static asserts */
-
-    static_assert(alignof(entry_seq) == CACHELINE_SIZE , "err");
-    static_assert(sizeof(entry_seq) == CACHELINE_SIZE * WRITE_BUCKET , "error align");
+    void recovery();
 };
 
 #endif
