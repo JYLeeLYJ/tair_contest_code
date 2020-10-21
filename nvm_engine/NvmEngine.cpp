acd8160f2f595c1049fbcda83a950080073358ba
JYLeeLYJ <JYLeeLYJ@qq.com>
2020-10-21 23:36:08 +0800

    [update]no recovery version

--- nvm_engine/NvmEngine.cpp
+++ nvm_engine/NvmEngine.cpp
@@ -7,6 +7,7 @@
 
 #include <libpmem.h>
 #include <sys/mman.h>
+#include <unistd.h>
 
 #include <cassert>
 
@@ -22,33 +23,9 @@ Status NvmEngine::CreateOrOpen(const std::string &name, DB **dbptr) {
     return Ok;
 }
 
-using namespace std::chrono;
-void test_single(void * p){
-    auto t1 = high_resolution_clock::now();
-    pmem_memset_persist(p , 0 , 16ULL * 1024 * 1024 * 1024);
-    auto t2 = high_resolution_clock::now();
-
-    Logger::instance().sync_log(fmt::format("single = {} ms", duration_cast<milliseconds>(t2 - t1).count()));
-}
-
-void test_multi(void * p){
-    constexpr size_t thread_cnt = 16;
-    constexpr size_t len =  16ULL * 1024 * 1024 * 1024 / thread_cnt;
-    std::thread ts[thread_cnt];
-    auto t1 = high_resolution_clock::now();
-    for(size_t i = 0 ; i< thread_cnt ; i ++){
-        ts[i] = std::thread([=]{
-            pmem_memset_persist((char *)(p) + i * len,  0 , len);
-        });
-    }
-    for(size_t i = 0 ; i < thread_cnt ; ++i){
-        ts[i].join();
-    }
-    auto t2 = high_resolution_clock::now();
-    Logger::instance().sync_log(fmt::format("single = {} ms", duration_cast<milliseconds>(t2 - t1).count()));
-}
-
 NvmEngine::NvmEngine(const std::string &name) {
+
+    bool is_exist = access(name.data() , 0);
 
     auto p = pmem_map_file(name.c_str(),NVM_SIZE,PMEM_FILE_CREATE, 0666, nullptr,nullptr);
     if(!p){
@@ -56,17 +33,118 @@ NvmEngine::NvmEngine(const std::string &name) {
         exit(0);
     }
 
-    Logger::instance().sync_log("*************test*****************");
-    test_single(p);
-    test_multi(p);
+    file = decltype(file){p , NVM_SIZE};
+
+    //TODO:check recovery
+    // if(is_exist)
+    //     recovery();
+
     Logger::instance().sync_log("*************init*****************");
 }
 
 Status NvmEngine::Get(const Slice &key, std::string *value) {
-    return Ok;
+
+    auto hash = hash_bytes_16(key.data());
+    auto head = search(key , hash);
+
+    if(head){
+        value->assign(reinterpret_cast<char *>(& file.value_blocks[head->beg]) , head->len);
+        return Ok;
+    }else{
+        return NotFound;
+    }
 }
 
 Status NvmEngine::Set(const Slice &key, const Slice &value) {
+
+    static thread_local uint32_t bucket_id = get_bucket_id() ;
+
+    auto hash = hash_bytes_16(key.data());
+    auto head = search(key , hash);
+
+    if(head)
+        return update(value , hash , head , bucket_id);
+    else 
+        return append(key,value , hash , bucket_id);
+
+}
+
+void NvmEngine::recovery(){
+    //TODO: 
+}
+
+head_info * NvmEngine::search(const Slice & key , uint64_t hash){
+
+    const auto prefix = *reinterpret_cast<const uint32_t *>(key.data());
+
+    for(auto & info : index.get_bucket(hash % index.bucket_cnt)){
+        auto & head = file.key_heads[info.index];
+        if(info.prefix == prefix && fast_key_cmp_eq(key.data() , head.key))
+            return & head;
+    }
+
+    return nullptr;
+}
+
+Status NvmEngine::update(const Slice & value , uint64_t hash , head_info * head , uint32_t bucket_id){
+    auto n_block = head->len / sizeof(value_block) + 1;
+    auto n_block_new = value.size() / sizeof(value_block) + 1;
+
+    uint32_t beg = allocator[bucket_id].allocate(n_block_new);
+    if(beg == decltype(allocator)::value_type::null_index) 
+        return OutOfMemory ;
+
+    allocator[bucket_id].deallocate(n_block , head->beg);
+
+    //direct copy
+    #ifdef LOCAL_TEST
+    memcpy(&(file.value_blocks[beg]) , value.data() , value.size());
+    head->beg = beg ;
+    head->len = n_block_new;
+    #else
+
+    auto new_head = *head ; 
+    new_head.beg = beg;
+    new_head.len = n_block_new;
+    
+    pmem_memcpy_persist(&(file.value_blocks[beg]) , value.data() , value.size());
+    pmem_memcpy_persist(head , &new_head , sizeof(head_info));
+    #endif
+
+    return Ok;
+}
+
+Status NvmEngine::append(const Slice & key , const Slice & value , uint64_t hash , uint32_t bucket_id){
+    auto n_block = value.size() / sizeof(value_block) + 1;
+    const auto prefix = *reinterpret_cast<const uint32_t *>(key.data());
+
+    //allocated seq and space
+    uint32_t value_beg = allocator[bucket_id].allocate(n_block);
+    if(value_beg == decltype(allocator)::value_type::null_index)
+        return OutOfMemory;
+    
+    uint32_t key_index = new_key_info(bucket_id);
+
+    //write data
+    auto & new_head = file.key_heads[key_index];
+    void * dst = &(file.value_blocks[value_beg]);
+
+    head_info head;
+
+    head.beg = value_beg;
+    head.len = value.size();
+    memcpy_avx_16(head.key , key.data());
+
+    #ifdef LOCAL_TEST
+    memcpy(&new_head , &head , sizeof(head_info));
+    memcpy(dst , value.data() , value.size());
+    #else
+    pmem_memcpy_persist(dst , value.data() , value.size());
+    pmem_memcpy_persist(&new_head , &head , sizeof(head_info));
+    #endif
+
+    index.insert(hash % HASH_SIZE , new_index_info(bucket_id) , key_index , prefix);
+
     return Ok;
 }
 
--- nvm_engine/NvmEngine.hpp
+++ nvm_engine/NvmEngine.hpp
@@ -9,6 +9,9 @@
 #include <atomic>
 
 #include "include/db.hpp"
+#include "include/kvfile.hpp"
+#include "include/hash_index.hpp"
+#include "include/allocator.hpp"
 
 class NvmEngine : DB {
 public:
@@ -25,14 +28,80 @@ public:
     ~NvmEngine();
 
 private:
+
+    static constexpr size_t META_SIZE = 1_KB;
+
     #ifdef LOCAL_TEST
-    static constexpr size_t NVM_SIZE = 64ULL * 1024 ;
+    static constexpr size_t DRAN_SIZE = 256_MB;
+    static constexpr size_t NVM_SIZE = 64_MB ;
+    static constexpr size_t KEY_AREA = 8_MB;
+    static constexpr size_t VALUE_AREA = NVM_SIZE - META_SIZE - KEY_AREA;
     #else
-    static constexpr size_t NVM_SIZE = 64ULL * 1024 * 1024 * 1024 ;
+    static constexpr size_t DRAM_SIZE = 8_GB;
+    static constexpr size_t NVM_SIZE = 64_GB ;
+    static constexpr size_t KEY_AREA = 8_GB ;
+    static constexpr size_t VALUE_AREA = NVM_SIZE - META_SIZE - KEY_AREA;
     #endif
+
+    static constexpr size_t N_KEY = KEY_AREA / sizeof(head_info) ;
+    static constexpr size_t N_VALUE = VALUE_AREA / sizeof(value_block);
+    static constexpr size_t N_IDINFO = N_KEY;
+
+    static constexpr size_t THREAD_CNT = 16;
+    static constexpr size_t BUCKET_CNT = THREAD_CNT;
+    static constexpr size_t HASH_SIZE = N_KEY /2;
+
+private:
+
+    struct alignas(CACHELINE_SIZE) bucket_info{
+        uint32_t index_info_seq{};
+        uint32_t key_seq{};
+    };
 
 private:
     void recovery();
+    head_info * search(const Slice & key , uint64_t hash);
+    Status update(const Slice & value , uint64_t hash , head_info * head , uint32_t bucket_id);
+    Status append(const Slice & key , const Slice & value , uint64_t hash , uint32_t bucket_id);
+
+    uint32_t get_bucket_id(){
+        return thread_seq ++ % BUCKET_CNT;
+    }
+
+    uint32_t new_index_info(uint32_t bucket_id){
+        constexpr auto n_index_per_bk = N_IDINFO / BUCKET_CNT;
+        return (bucket_infos[bucket_id].index_info_seq ++) + bucket_id * n_index_per_bk;
+    }
+
+    uint32_t new_key_info(uint32_t bucket_id){
+        constexpr auto n_key_per_bk = N_KEY / BUCKET_CNT;
+        auto seq = bucket_infos[bucket_id].key_seq ++;
+        return seq < n_key_per_bk ? seq + bucket_id * n_key_per_bk : index.null_id;
+    }
+
+    // uint32_t new_value_blocks(uint32_t bucket_id , uint32_t n){
+    //     constexpr auto n_block_per_bk = N_VALUE/ BUCKET_CNT;
+    //     auto seq = bucket_infos[bucket_id].value_block_seq;
+    //     //allocator
+    //     bucket_infos[bucket_id].value_block_seq += n;
+    //     return bucket_infos[bucket_id].value_block_seq < n_block_per_bk ? seq + bucket_id * n_block_per_bk : index.null_id;
+    // }
+
+private:
+
+    kv_file_info<N_KEY , N_VALUE> file;
+    std::atomic<uint32_t> thread_seq{0};
+
+    alignas(CACHELINE_SIZE)
+    std::array<bucket_info , BUCKET_CNT> bucket_infos{};
+    alignas(CACHELINE_SIZE)
+    std::array<value_block_allocator<N_KEY / BUCKET_CNT> , BUCKET_CNT> allocator;
+
+    hash_index<HASH_SIZE , N_IDINFO> index;
+
+    static_assert(sizeof(bucket_info) == 64 , "");
+    static_assert(sizeof(bucket_infos) == 1_KB , "");
+
 };
 
 #endif
